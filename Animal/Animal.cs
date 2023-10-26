aac0edf754c58c194f496f1e7c522cbbb4e12f7a
Dommss <Dommterr@gmail.com>
2023-10-26 11:23:59 +0200

    Created more debug information and ability to grab the animal

--- Animal/Animal.cs
+++ Animal/Animal.cs
@@ -6,11 +6,21 @@ public partial class Animal : RigidBody2D {
     private VisibleOnScreenNotifier2D _notifier;
 
     private bool _isDead = false;
+    private bool _isDragging = false;
+    private bool _isReleased = false;
+    private Vector2 _start = Vector2.Zero;
+    private Vector2 _dragStart = Vector2.Zero;
+    private Vector2 _draggedVector = Vector2.Zero;
+    private Vector2 _lastDraggedPos = Vector2.Zero;
+    private float _lastDraggedAmount = 0f;
+    private float _firedTime = 0f;
     public override void _Ready() {
         _signalManager = GetNode<SignalManager>("/root/SignalManager");
 
         _notifier = GetNode<VisibleOnScreenNotifier2D>("VisibleOnScreenNotifier2D");
 
+        _start = GlobalPosition;
+        
         _notifier.ScreenExited += OnScreenExited;
         InputEvent += OnInputEvent;
     }
@@ -27,15 +37,26 @@ public partial class Animal : RigidBody2D {
     private void UpdateDebugLabel() {
         var s = "g_pos: " + Utilities.VectorToString(GlobalPosition) + "\n";
 
-        s += "ang: " + AngularVelocity.ToString("F1") + "\n";
-        s += "linear: " + Utilities.VectorToString(LinearVelocity);
-
+        s += "ang: " + AngularVelocity.ToString("F1") + " - linear: " + Utilities.VectorToString(LinearVelocity) + "\n";
+        s += "dragging: " + _isDragging + " - released: " + _isReleased + " - firedTime: " + _firedTime + "\n";
+        s += "start: " + Utilities.VectorToString(_start) + " - dragStart: " + Utilities.VectorToString(_dragStart) + "\n";
+        s += "lastDraggedPos: " + Utilities.VectorToString(_lastDraggedPos) + " - lastDraggedAmount: " +
+             _lastDraggedAmount;
+            
         _signalManager.EmitSignal(SignalManager.SignalName.UpdateDebugLabel, s);
     }
 
+    private void GrabIt() {
+        _isDragging = true;
+        _dragStart = GetGlobalMousePosition();
+        _lastDraggedPos = _dragStart;
+    }
+
     private void OnInputEvent(Node viewport, InputEvent @event, long shapeIdx) {
+        if (_isDragging || _isReleased) return;
+        
         if (@event.IsActionPressed("Drag")) {
-         GD.Print(@event); 
+            GrabIt();
         }
     }
 
